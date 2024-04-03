edb17beef828b627a434ce597fcd774ebdae267f
Yash Choudhari <yash.10712082@ltimindtree.com>
2024-04-03 14:50:22 +0530

    seperate two para side by side

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ function App() {
       </div>
       <h1>Vite + React created</h1>
       <h2>with CI/CD integrated end to end</h2>
-      <div class="parent"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A aut temporibus doloribus molestias beatae debitis consequatur nisi veniam, provident cupiditate animi eligendi voluptate atque dolore. Quos itaque vitae asperiores harum aut voluptatum dolorem officiis numquam!</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi asperiores beatae exercitationem natus laudantium nisi aliquam et eos nostrum, sunt illum soluta veniam nobis velit, vero mollitia ab placeat obcaecati minus fugit. Qui quas expedita quia voluptate quae odio fugit.</p></div>
+      <div class="parent"><p class="child">Lorem ipsum dolor sit amet consectetur adipisicing elit. A aut temporibus doloribus molestias beatae debitis consequatur nisi veniam, provident cupiditate animi eligendi voluptate atque dolore. Quos itaque vitae asperiores harum aut voluptatum dolorem officiis numquam!</p>
+        <p class="child">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi asperiores beatae exercitationem natus laudantium nisi aliquam et eos nostrum, sunt illum soluta veniam nobis velit, vero mollitia ab placeat obcaecati minus fugit. Qui quas expedita quia voluptate quae odio fugit.</p></div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
